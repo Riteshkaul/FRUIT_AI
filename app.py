37668b030196a763f4a19fccc307715f1f6b0440
Ritesh <riteshkaul682@gmail.com>
2024-09-16 00:47:00 +0530

    bug fix

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ def delete_faq(id):
     return jsonify({"message": "FAQ deleted"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
